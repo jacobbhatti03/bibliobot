3d545ad003f4edab82ae4c21e5370c9622fc830e
jacobbhatti03 <jacobbhatti70@gmail.com>
2025-12-22 14:35:01 +0500

    Delete auth.py

--- auth.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def get_user():
-    # Skeleton mode: no login required yet
-    return None
-
-def sidebar_login_ui():
-    with st.sidebar:
-        st.caption("Auth: disabled (skeleton mode)")
